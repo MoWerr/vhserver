89882977f10f69a5f94f11c16db066c49d786475
MoWer <mbaran2601@gmail.com>
2021-04-25 12:44:53 +0200

    Added dev root

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -29,7 +29,22 @@ To debug in IntelliJ Idea, open the 'Maven Projects' tool window (View
 
 version = "2020.2"
 
+object DevRoot : GitVcsRoot({
+    id("Dev")
+    name = "Dev"
+    url = "https://github.com/MoWerr/vhserver"
+    branch = "refs/heads/dev"
+    userForTags = "MoWer"
+    authMethod = password {
+        userName = "MoWerr"
+        password = "credentialsJSON:c936cbbd-a1d6-4a1d-87c0-48f933779af3"
+    }
+    param("oauthProviderId", "PROJECT_EXT_2")
+}
+
 project {
+    vcsRoot(DevRoot)
+
     subProject(Stable)
     subProject(Dev)
 }
