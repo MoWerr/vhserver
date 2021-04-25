498a7e2eee05a043bdada7fb87df1e4ee8d8a804
MoWer <mbaran2601@gmail.com>
2021-04-25 12:06:17 +0200

    Initial clean-up

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -30,36 +30,50 @@ To debug in IntelliJ Idea, open the 'Maven Projects' tool window (View
 version = "2020.2"
 
 project {
-
-    vcsRoot(HttpsGithubComMoWerrVhserverRefsHeadsDev)
-    vcsRoot(HttpsGithubComMoWerrVhserverRefsHeadsDev1)
-    subProjectsOrder = arrayListOf(RelativeId("Stable"), RelativeId("Dev"))
-
     subProject(Stable)
-    subProject(Dev)
+    //subProject(Dev)
 }
 
-object HttpsGithubComMoWerrVhserverRefsHeadsDev : GitVcsRoot({
-    name = "https://github.com/MoWerr/vhserver#refs/heads/dev"
-    url = "https://github.com/MoWerr/vhserver"
-    branch = "refs/heads/dev"
-    branchSpec = "refs/heads/*"
+object Stable : Project({
+    name = "Stable"
+
+    buildType(Build)
 })
 
-object HttpsGithubComMoWerrVhserverRefsHeadsDev1 : GitVcsRoot({
-    name = "https://github.com/MoWerr/vhserver#refs/heads/dev (1)"
-    url = "https://github.com/MoWerr/vhserver"
-    branch = "refs/heads/dev"
-    branchSpec = "refs/heads/*"
-    userForTags = "Artemis"
-    authMethod = password {
-        userName = "MoWerr"
-        password = "credentialsJSON:c936cbbd-a1d6-4a1d-87c0-48f933779af3"
+object Build : BuildType({
+    name = "Build"
+
+    vcs {
+        root(DslContext.settingsRoot)
+    }
+
+    steps {
+        dockerCommand {
+            name = "Build image"
+            commandType = build {
+                source = file {
+                    path = "Dockerfile"
+                }
+                namesAndTags = "mowerr/vhserver:latest"
+            }
+            param("dockerImage.platform", "linux")
+        }
+
+        dockerCommand {
+            name = "Push image"
+            commandType = push {
+                namesAndTags = "mowerr/vhserver:latest"
+            }
+        }
+    }
+
+    triggers {
+        vcs {
+        }
     }
 })
 
-
-object Dev : Project({
+/*object Dev : Project({
     name = "Dev"
 
     buildType(PromoteToStable)
@@ -126,58 +140,4 @@ object PromoteToStable : BuildType({
         }
     }
 })
-
-
-object Stable : Project({
-    name = "Stable"
-
-    vcsRoot(Stable_HttpsGithubComMoWerrVhserverRefsHeadsMain)
-
-    buildType(Build)
-})
-
-object Build : BuildType({
-    name = "Build"
-
-    vcs {
-        root(DslContext.settingsRoot)
-    }
-
-    steps {
-        dockerCommand {
-            name = "Build image"
-            commandType = build {
-                source = file {
-                    path = "Dockerfile"
-                }
-                namesAndTags = "mowerr/vhserver:latest"
-            }
-            param("dockerImage.platform", "linux")
-        }
-        dockerCommand {
-            name = "Push image"
-            commandType = push {
-                namesAndTags = "mowerr/vhserver:latest"
-            }
-        }
-    }
-
-    triggers {
-        vcs {
-            quietPeriodMode = VcsTrigger.QuietPeriodMode.USE_CUSTOM
-            quietPeriod = 30
-            branchFilter = "+:<default>"
-        }
-    }
-})
-
-object Stable_HttpsGithubComMoWerrVhserverRefsHeadsMain : GitVcsRoot({
-    name = "https://github.com/MoWerr/vhserver#refs/heads/main"
-    url = "https://github.com/MoWerr/vhserver"
-    branch = "refs/heads/main"
-    branchSpec = "refs/heads/*"
-    authMethod = password {
-        userName = "MoWerr"
-        password = "credentialsJSON:c936cbbd-a1d6-4a1d-87c0-48f933779af3"
-    }
-})
+*/
