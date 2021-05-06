94090bafac7f8b93df8ff0528a250702b1b88dfc
MoWer <mbaran2601@gmail.com>
2021-05-06 23:53:16 +0200

    failed healthchecks will be stored inside the cronjobs log file

--- root/healthcheck.sh
+++ root/healthcheck.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 source /common.sh
 
-s6-setuidgid husky vhserver monitor || exit 1
+s6-setuidgid husky vhserver monitor
+
+if [[ $? != 0 ]]; then
+  echo "[$(date "+%d/%m/%y %H:%M:%S")] Server was down during last healthcheck!" >> /data/cronjobs.log
+  exit 1
+else
+  exit 0
+fi
